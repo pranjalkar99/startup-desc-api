5a7792a1a95490d8d0886b2c1e0d5522bef99918
PRANJAL KAR <74347116+pranjalkar99@users.noreply.github.com>
2024-10-19 16:05:55 +0000

    :wrench: Added 2nd API for Mapping different port

--- mapping.py
+++ mapping.py
@@ -106,4 +106,4 @@ async def map_questions(request: RequestBody):
 # Example usage
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get('PORT', 8000)))
